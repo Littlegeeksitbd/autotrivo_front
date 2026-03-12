43d134a7b590c0a88b8533f6412e667a5c674f08
applandsys <applandsys@gmail.com>
2026-03-12 17:08:24 +0600

    feat: Production config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:5000";
-export const redirectUrl = "http://localhost:3001";
+export const baseUrl = "https://api.autotrivo.com";
+export const redirectUrl = "https://app.autotrivo.com";

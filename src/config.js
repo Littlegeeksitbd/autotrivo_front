1aa61b9e7a888ed81d711c8f2da91eb40f9e88f8
applandsys <applandsys@gmail.com>
2026-03-29 12:35:37 +0600

    fix: Autotrivo Landing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,2 @@
-// export const baseUrl =  "http://localhost:5000"; // "https://api.autotrivo.com";
-// export const redirectUrl = "http://localhost:3001"; //"https://app.autotrivo.com";
-
 export const baseUrl =  "https://api.autotrivo.com";
 export const redirectUrl = "https://app.autotrivo.com";
